45a8db6cf602c64d7ac06a8de3ea96dfc1a8201e
Jaden Carver <jaden.carver@gmail.com>
2012-03-16 03:53:21 +0000

    fixed for blocks

--- lib/method_access.rb
+++ lib/method_access.rb
@@ -4,7 +4,7 @@ require "hash_ext"
 module MethodAccess
   class Hash < ::Hash
     def method_missing(key, *args, &block)
-      args += block if block_given?
+      args << block if block_given?
       key = [key] + args if args.any?
       self[key]
     end
